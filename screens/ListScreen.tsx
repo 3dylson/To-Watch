65f227b3a51ba1e23d380112c57f65bef74a2398
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-04-02 16:40:33 +0100

    addWrapper

--- screens/ListScreen.tsx
+++ screens/ListScreen.tsx
@@ -1,11 +1,14 @@
 import { HeaderStyleInterpolators } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, } from 'react-native';
 
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 import Serie from '../components/Serie';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 
-export default function TabOneScreen() {
+export default function ListScreen() {
+  const [serie, setSerie] = React.useState();
+
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
@@ -19,7 +22,17 @@ export default function TabOneScreen() {
 
       {/*Write a serie*/}
 
-      
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}>
+          <TextInput style={styles.input} placeholder={'What are you watching?'}/>
+
+          <TouchableOpacity>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
 
     </View>
   );
@@ -50,4 +63,34 @@ const styles = StyleSheet.create({
 items:{
   marginTop: 30,
 },
+writeTaskWrapper:{
+  position: 'absolute',
+  bottom: 60,
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center'
+},
+input:{
+  paddingVertical: 15,
+  paddingHorizontal: 15,
+  backgroundColor: '#FFF',
+  borderRadius: 60,
+  borderColor: '#C0C0C0',
+  borderWidth: 1,
+  width: 250,
+},
+addWrapper:{
+  width: 60,
+  height: 60,
+  backgroundColor: '#FFF',
+  borderRadius: 60,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderColor: '#C0C0C0',
+  borderWidth: 1,
+
+},
+addText:{},
+
 });
